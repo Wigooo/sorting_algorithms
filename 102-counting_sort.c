905d906c4bb1a4ed17e668884ad4fd191110ffd4
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-12-18 15:01:40 -0500

    updates

---
+++ 102-counting_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * counting_sort - sorts an array of integers in ascending
+ *			order using the Counting sort algorithm
+ * @array: array of integers
+ * @size: number of integers
+ * Return: print your counting array once it is set up
+ */
+void counting_sort(int *array, size_t size)
+{
+	int maximum, number, i, l, *count, *out;
+	size_t index, k, m, n;
+
+	if (size < 2)
+		return;
+	maximum = array[0];
+	for (index = 1; index < size; index++)
+		if (array[index] > maximum)
+			maximum = array[index];
+	count = malloc(sizeof(size_t) * (maximum + 1));
+	out = malloc(sizeof(int) * size);
+	for (i = 0; i <= maximum; i++)
+		count[i] = 0;
+	for (k = 0; k < size; k++)
+	{
+		number = array[k];
+		count[number] += 1;
+	}
+	for (l = 1; l <= maximum; l++)
+		count[l] += count[l - 1];
+	print_array(count, maximum + 1);
+	for (m = 0; m < size; m++)
+	{
+		out[count[array[m]] - 1] = array[m];
+		count[array[m]]--;
+	}
+	for (n = 0; n < size; n++)
+		array[n] = out[n];
+
+	free(count);
+	free(out);
+}
---
+++ 103-merge_sort.c
@@ -0,0 +1,74 @@
+#include "sort.h"
+
+/**
+ * merging_babyarrays - merging subarray
+ * @baby: subarray of main array
+ * @buffer: buffer to store in sorted baby array
+ * @first: front index of array
+ * @middle: middle index of array
+ * @last: back index of array
+ */
+void merging_babyarray(int *baby, int *buffer, size_t first,
+		size_t middle, size_t last)
+{
+	size_t x, y, i = 0;
+
+	printf("Merging...\n[left]: ");
+	print_array(baby + first, middle - first);
+
+	printf("[right]: ");
+	print_array(baby + middle, last - middle);
+
+	for (x = first, y = middle; x < middle && y < last; i++)
+		buffer[i] = (baby[x] < baby[y]) ? baby[x++] : baby[y++];
+	for (; x < middle; x++)
+		buffer[i++] = baby[x];
+	for (; y < last; y++)
+		buffer[i++] = baby[y];
+	for (x = first, i = 0; x < last; x++)
+		baby[x] = buffer[i++];
+
+	printf("[Done]: ");
+	print_array(baby + first, last - first);
+}
+
+/**
+ * merge_recursion - Implement the merge sort algorithm through recursion.
+ * @baby: A subarray of an array
+ * @buffer: A buffer to store the sorted result.
+ * @first: The front index of the subarray.
+ * @last: The back index of the subarray.
+ */
+void merge_recursion(int *baby, int *buffer, size_t first, size_t last)
+{
+	size_t middle;
+
+	if (last - first > 1)
+	{
+		middle = first + (last - first) / 2;
+		merge_recursion(baby, buffer, first, middle);
+		merge_recursion(baby, buffer, middle, last);
+		merging_babyarray(baby, buffer, first, middle, last);
+	}
+}
+
+/**
+ * merge_sort - sorts an array of integers in ascending order
+ *			using the Merge sort algorithm
+ * @array: array of integers
+ * @size: number of integers
+ * Return: merged sorted output
+ */
+void merge_sort(int *array, size_t size)
+{
+	int *buffer;
+
+	if (array == NULL || size < 2)
+		return;
+	buffer = malloc(sizeof(int) * size);
+	if (buffer == NULL)
+		return;
+
+	merge_recursion(array, buffer, 0, size);
+	free(buffer);
+}
